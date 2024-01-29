9b734299903c1120a0f213158f0878a7cf2e3f98
love-to-code108 <love.to.code108@gmail.com>
2024-01-29 20:54:33 +0530

    the navbar is more or less done for the desktop version

--- website/src/screenSize/Desktop/Components/_0_navbar.jsx
+++ website/src/screenSize/Desktop/Components/_0_navbar.jsx
@@ -3,9 +3,39 @@
 // CSS
 import "../Pages/homePage.css"
 
+// ASSETS
+import tecxaroLogo from "../../../assets/svg/tecxaro-logo.svg"
+
+
 export const Navbar = () => {
   return (
-    <div id="Navbar">
+    <div id="Navbar" className=" drop-shadow-lg">
+
+
+
+
+      {/* TECXARO LOGO */}
+      <img src={tecxaroLogo} alt="the logo of tecxaro" className=" w-[9rem] ml-24" />
+
+
+
+
+
+
+      {/* JOIN US BUTTON */}
+      <div className="joinUs-navbar border-2 border-white px-7 py-3">
+
+
+        <p className="poppins  text-[#F56B00] text-3xl font-extrabold">Join
+
+          <span className=" text-white"> Us</span>
+
+        </p>
+      </div>
+
+
+
+
 
     </div>
   )
--- website/src/screenSize/Desktop/Pages/homePage.jsx
+++ website/src/screenSize/Desktop/Pages/homePage.jsx
@@ -1,12 +1,13 @@
 
 
 // CSS
+import { Navbar } from "../Components/_0_navbar"
 import "./homePage.css"
 
 export const HomePageDesktop = () => {
   return (
-    <div>
-
+    <div id="home-page-desktop-id" className=" bg-[#2A2A2A]">
+        <Navbar/>
     </div>
   )
 }
