7bb05f3bad25318f6cae005f4c2591e9eeeaf835
love-to-code108 <love.to.code108@gmail.com>
2024-01-29 23:31:14 +0530

    the hero section is more or less done for the desktop version

--- website/src/screenSize/Desktop/Components/_1_heroSection.jsx
+++ website/src/screenSize/Desktop/Components/_1_heroSection.jsx
@@ -1,4 +1,6 @@
-
+// ASSETS
+import mouse from "../../../assets/svg/mouse.svg"
+import downArrow from "../../../assets/svg/downArrow.svg"
 
 
 // CSS
@@ -11,25 +13,35 @@ export const HeroSection = () => {
 
 
       {/* THE MAIN BIG BOLD WRITING */}
-      <div className=" raleway text-white mb-[10rem] font-medium">
+      <div className=" raleway text-white mb-[12rem] font-medium flex flex-col justify-center items-center">
 
 
         {/* FIRST LINE */}
         <h1 className="heroSection-main-highlight">
 
-          Discover <span>robotics</span> through</h1>
+          Discover <span className=" text-[#F56B00] font-bold">robotics</span> through</h1>
 
 
         {/* SECOND LINE */}
-        <h1 className="heroSection-main-highlight">
+        <h1 className="heroSection-main-highlight mb-4">
 
-          easy <span>practical</span> lessons </h1>
+          easy <span className=" text-[#F56B00] font-bold">practical</span> lessons </h1>
+
+
+
+
+        {/* THE SMALL BOLD WRITING  */}
+
 
 
         {/* THE SMALL LINES SUBS */}
-        <p className=" poppins">
+        <p className=" poppins font-semibold text-xl w-[61rem]">
 
-          Join us on a pathway to excellence, where practical, technical, and experiential learning converge to shape your skills and empower your mind for real-world success.</p>
+          Join us on a pathway to excellence, where
+          <span className=" text-[#F56B00] font-bold"> practical </span>,
+          <span className=" text-[#F56B00] font-bold"> technical </span>and
+          <span className=" text-[#F56B00] font-bold"> experiential learning </span>
+          converge to shape your skills and empower your mind for real-world success.</p>
 
 
       </div>
@@ -37,13 +49,21 @@ export const HeroSection = () => {
 
 
 
-      {/* THE SMALL BOLD WRITING  */}
+      
 
 
 
 
 
       {/* THE SMALL SVG ANIMATION TO SCROLL */}
+      <div id="scrolling-down-animation-hero-section" className=" absolute bottom-8">
+
+          <img src={mouse} alt="" className="" />
+          <img src={downArrow} alt="" className=" animate-bounce relative top-4" />
+
+      </div>
+
+
     </div>
   )
 }
