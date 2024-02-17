6ca8789b48e5753719d3042b15f88b08e1482ac3
love-to-code108 <love.to.code108@gmail.com>
2024-02-17 17:23:23 +0530

    the project section cards are done now i am giving the final touches to complete it

--- website/src/screenSize/Desktop/Components/_7_ourProjects.jsx
+++ website/src/screenSize/Desktop/Components/_7_ourProjects.jsx
@@ -9,6 +9,8 @@ export const ProjectsSection = () => {
 
 
       <div id="PS-wrapper">
+
+
         {/* OUR PROJECTS HEADING */}
         <div className="text-8xl raleway font-semibold mb-20">
           <h1 className=" text-white">Our <span className=" color-orange">Projects</span></h1>
@@ -16,38 +18,118 @@ export const ProjectsSection = () => {
 
 
         {/* WRAPPER AROUND THE THREE CARDS ABOUT OUR PROJECTS */}
-        <div>
+        <div className="PS-wrapper-project-cards">
+
+
+
+
+
 
 
 
           {/* CARD 1 */}
-          <div>
+          <div className="PS-project-card-main">
 
-              {/* PROJECT IMAGE */}
-              <img className="PS-projectImg-edit" src={projectImg} alt="" />
+            {/* PROJECT IMAGE */}
+            <img className="PS-projectImg-edit" src={projectImg} alt="" />
+
+
+            {/* TEXT HEADING */}
+            <div className=" flex w-[100%] raleway text-3xl font-semibold my-4">
+              <h1 className="">Line Follower</h1>
+            </div>
 
 
 
-              {/* TEXT */}
-              <div>
-                <p>A line follower project makes a robot follow marked paths using sensors to detect lines and adjust its movement. It showcases basic autonomous navigation through sensor integration and programming.</p>
+            {/* TEXT */}
+            <div className=" dmMono PS-project-card-para">
+              <p>A line follower project makes a robot follow marked paths using sensors to detect lines and adjust its movement. It showcases basic autonomous navigation through sensor integration and programming.</p>
+            </div>
+
+
+            {/* THE LEARN MORE BUTTON */}
+            <div className=" flex w-[100%] justify-end">
+              <div className="PS-project-card-learnMore-button text-2xl px-2 py-2">
+                Learn More
               </div>
-
-
-              {/* THE LEARN MORE BUTTON */}
+            </div>
 
 
           </div>
+
+
+
+
+
+
+
 
 
           {/* CARD 2 */}
-          <div>
+          <div className="PS-project-card-main">
+
+            {/* PROJECT IMAGE */}
+            <img className="PS-projectImg-edit" src={projectImg} alt="" />
+
+
+            {/* TEXT HEADING */}
+            <div className=" flex w-[100%] raleway text-3xl font-semibold my-4">
+              <h1 className="">Line Follower</h1>
+            </div>
+
+
+
+            {/* TEXT */}
+            <div className=" dmMono PS-project-card-para">
+              <p>A line follower project makes a robot follow marked paths using sensors to detect lines and adjust its movement. It showcases basic autonomous navigation through sensor integration and programming.</p>
+            </div>
+
+
+            {/* THE LEARN MORE BUTTON */}
+            <div className=" flex w-[100%] justify-end">
+              <div className="PS-project-card-learnMore-button text-2xl px-2 py-2">
+                Learn More
+              </div>
+            </div>
+
 
           </div>
 
 
+
+
+
+
+
+
+
           {/* CARD 3 */}
-          <div>
+          <div className="PS-project-card-main">
+
+            {/* PROJECT IMAGE */}
+            <img className="PS-projectImg-edit" src={projectImg} alt="" />
+
+
+            {/* TEXT HEADING */}
+            <div className=" flex w-[100%] raleway text-3xl font-semibold my-4">
+              <h1 className="">Line Follower</h1>
+            </div>
+
+
+
+            {/* TEXT */}
+            <div className=" dmMono PS-project-card-para">
+              <p>A line follower project makes a robot follow marked paths using sensors to detect lines and adjust its movement. It showcases basic autonomous navigation through sensor integration and programming.</p>
+            </div>
+
+
+            {/* THE LEARN MORE BUTTON */}
+            <div className=" flex w-[100%] justify-end">
+              <div className="PS-project-card-learnMore-button text-2xl px-2 py-2">
+                Learn More
+              </div>
+            </div>
+
 
           </div>
 
