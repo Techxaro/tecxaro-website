c8269aafd2003c8e44e99a4d753b803dfbd20fd8
love-to-code108 <love.to.code108@gmail.com>
2024-02-21 19:18:50 +0530

    the why choose us part 1 that is the pratical approach section of the website for the mobile version is done

--- website/src/screenSize/Mobile/Components/_3_practicalApproach-M.jsx
+++ website/src/screenSize/Mobile/Components/_3_practicalApproach-M.jsx
@@ -13,34 +13,34 @@ export const WhyChooseUs = () => {
 
 
             {/* WHY CHOOSE US HEADING */}
-            <div className="">
-                <h1>Why choose <span>Us ?</span></h1>
+            <div className="raleway WCU-M-heading font-semibold mb-6">
+                <h1>Why choose <span className=" color-orange">Us ?</span></h1>
             </div>
 
 
             {/* WHY CHOOSE US FIRST PARAGRAPH */}
-            <div>
-                <p>In Tecxaro, we believe in learning by doing. Our approach is hands-on and practical, guiding you to create real-world projects using ESP32 boards. Skip traditional methods; join us to innovate and build with confidence in the exciting field of robotics.</p>
+            <div className=" WCU-M-firstParagraph mb-6 normal-z">
+                <p className=" dmMono">In Tecxaro, we believe in <span className="text-orange-highlight">learning by doing</span>. Our approach is <span className=" text-orange-Bhighlight">hands-on</span> and practical, guiding you to create real-world projects using ESP32 boards. Skip traditional methods; join us to innovate and build with confidence in the exciting field of robotics.</p>
             </div>
 
 
 
             {/* SMALL ORANGE CIRCLE */}
-            <div className="WCU-small-orange-circle">
+            <div className="WCU-small-orange-circle circle-z">
             </div>
 
 
 
             {/* PRACTICAL APPROACH SUBHEADING */}
-            <div>
-                <h1>Practical <span>Approach</span></h1>
+            <div className=" w-[100%] WCU-M-practicalApproach-subHeading raleway font-semibold mb-4 normal-z">
+                <h1>Practical <span className=" color-orange">Approach</span></h1>
             </div>
 
 
 
 
             {/* ESP 32 IMAGE */}
-            <div>
+            <div className=" normal-z">
                 <img src={esp32MobileVersion} alt="" />
             </div>
         </div>
