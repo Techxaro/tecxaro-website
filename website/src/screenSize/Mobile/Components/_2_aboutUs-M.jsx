bacfe1f011fdf7316dda7711cd15e6cd7e5948f2
love-to-code108 <love.to.code108@gmail.com>
2024-02-21 16:26:52 +0530

    the about us section of the website for the mobile version is more or less done

--- website/src/screenSize/Mobile/Components/_2_aboutUs-M.jsx
+++ website/src/screenSize/Mobile/Components/_2_aboutUs-M.jsx
@@ -15,30 +15,53 @@ export const AboutSectionMobile = () => {
             
             
             {/* THE ORANGE CIRCLE */}
-            <div className="ASM-big-orange-circle"></div>
+            <div className="ASM-big-orange-circle circle-z"></div>
+
+
+
 
 
 
             {/* ABOUT US SECTION IMAGE */}
-            <div>
+            <div className=" normal-z mb-8">
                 <img src={aboutUsSectionImg} alt="" />
             </div>
 
 
 
+
+
+
+
             {/* THE SMALL ORANGE CIRCLE */}
-            <div className="ASM-small-orange-circle">
+            <div className="ASM-small-orange-circle circle-z">
             </div>
 
 
 
+
+
+
+
             {/* ABOUT US HEADING */}
+            <div className=" normal-z w-[100%] raleway mb-8">
+                <h1 className=" ASM-aboutUs-headingSection font-semibold">About <span className="color-orange">Us</span></h1>
+            </div>
+
+
+
+
 
 
 
 
             {/* ABOUT US PARAGRAPH */}
-
+            <div className=" normal-z">
+                <p className=" dmMono ASM-paragraphSection ">
+                At Tecxaro, <span className=" text-orange-Bhighlight">we make learning exciting</span> and effective with a <span className="text-orange-highlight">simple</span> yet <span className="text-orange-highlight">impactful</span> approach prioritizing hands-on practical learning. True mastery comes from doing, so we immerse students in a world of touching, building, and experimenting with robotics. From crafting innovative projects to solving real-world problems, our hands-on approach ensures each lesson leaves a lasting mark. Join us to discover the joy of <span className="text-orange-highlight">learning through doing</span>, where theory meets practice, and your creativity flourishes. At Tecxaro, we 
+                don&apos;t just teach – we empower you to create and innovate.
+                </p>
+            </div>
 
         </div>
 
