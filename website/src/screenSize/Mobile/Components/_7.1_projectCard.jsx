106f5b751a63d1b0d52e8bf5ab8d850e3da709b8
love-to-code108 <love.to.code108@gmail.com>
2024-02-22 20:24:56 +0530

    the project cards for the different projects are more or less done

--- website/src/screenSize/Mobile/Components/_7.1_projectCard.jsx
+++ website/src/screenSize/Mobile/Components/_7.1_projectCard.jsx
@@ -6,45 +6,47 @@ import "../Pages/homePageMobile.css"
 
 
 export const ProjectCard = (props) => {
-  return (
-    <div id='PC-M-main'>
-        
+    return (
+        <div id='PC-M-main'>
 
-        {/* WRAPPER */}
-        <div id='PC-M-wrapper'>
 
-            {/* PROJECT IMAGE */}
-            <div>
-                <img src={props.img} alt="" />
+            {/* WRAPPER */}
+            <div id='PC-M-wrapper'>
+
+                {/* PROJECT IMAGE */}
+                <div>
+                    <img className='PC-M-projectImg' src={props.img} alt="" />
+                </div>
+
+
+                {/* PROJECT NAME */}
+                <div className=' w-[100%] raleway font-semibold mb-4 mt-2'>
+                    <h1 className='PC-M-projectHeading'>{props.projHeading}</h1>
+                </div>
+
+
+                {/* PROJECT PARA */}
+                <div className=' dmMono mb-6'>
+                    <p className='PC-M-projectParagraph'>{props.projPara}</p>
+                </div>
+
+
+                {/* LEARN MORE BUTTON */}
+                <div className=' w-[100%] flex justify-end'>
+                    <div className='PC-M-learnMoreButton'>
+                        <p className=' dmMono'>Learn more</p>
+                    </div>
+                </div>
+
             </div>
-
-
-            {/* PROJECT NAME */}
-            <div className=' w-[100%] raleway font-semibold'>
-                <h1 className='PC-M-projectHeading'>{props.projHeading}</h1>
-            </div>
-
-
-            {/* PROJECT PARA */}
-            <div className=' dmMono'>
-                <p>{props.projPara}</p>
-            </div>
-
-
-            {/* LEARN MORE BUTTON */}
-            <div>
-
-            </div>
-
         </div>
-    </div>
-  )
+    )
 }
 
 
 ProjectCard.propTypes = {
-    projHeading:PropTypes.string,
-    projPara:PropTypes.string,
-    img:PropTypes.any,
-  };
+    projHeading: PropTypes.string,
+    projPara: PropTypes.string,
+    img: PropTypes.any,
+};
 
